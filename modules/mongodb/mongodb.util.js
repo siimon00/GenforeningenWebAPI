3d33249de5596879ca21c46f0b5c765facfbffd4
Simon Hansen <osterbysimon@hotmail.com>
2019-06-12 21:40:16 +0200

    Update mongodb.util.js

--- modules/mongodb/mongodb.util.js
+++ modules/mongodb/mongodb.util.js
@@ -10,7 +10,7 @@
     function init() {
         //mongoose.connect('mongodb://localhost:27017/genforeningen', { useNewUrlParser: true, useFindAndModify: false });
         //"mongodb://reunion2020-db:kvIIant8vSvFYI34bKjB9xbdqFWrFFAn1g5fnflfPMeKOuKiQsXUrN7cGdF0oGoqSQL9MghDWPu4nK8l8kWh9Q%3D%3D@reunion2020-db.documents.azure.com:10255/?ssl=true"
-        mongoose.connect('mongodb://genforeningen2020-db:' + encodeURIComponent('FSj118DiOUgqMEhAVqywCvpoGs2mOPtBDUuoWuZ7K05b9UpSsskQiZub2LGKfkY4loje65YqiP8a9nJNSIL7gQ==') + '@genforeningen2020-db.documents.azure.com:10255/?ssl=true',
+        mongoose.connect('mongodb://genforeningen-api:' + encodeURIComponent('kELmUSwNHiuw3d4vuNZ6Eix0SEUMq2XezIIIRVoqEQSfzMJLlci3aYU5MZ27XFxD9GF3mx3GYjikyLQUKIPsIw==') + '@genforeningen-api.documents.azure.com:10255/?ssl=true',
          {
               useNewUrlParser: true,
               useFindAndModify: false,
